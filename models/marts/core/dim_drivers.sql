5a4ade4b44451413f2dcf918f7b97d707b190e32
Rei Chavez <reic91011@gmail.com>
2023-10-05 22:33:04 +0000

    lifetime_churn fix

--- models/marts/core/dim_drivers.sql
+++ models/marts/core/dim_drivers.sql
@@ -32,18 +32,29 @@ driver_rides as (
 
     select
         rides.driver_id,
-
-        min(events.timestamp) as first_ride_date,
-        max(events.timestamp) as most_recent_ride_date,
+        min(DATE_TRUNC(events.timestamp, day)) as first_ride_date,
+        max(DATE_TRUNC(events.timestamp, day)) as most_recent_ride_date,
         count(rides.ride_id) as number_of_rides,
-        ROUND(SUM(fact_rides.total), 2) as lifetime_value
+        ROUND(SUM(fact_rides.total), 2) as revenue
 
     from fact_rides
     join rides using (ride_id)
     join events using (ride_id)
 
-
     group by 1
+
+),
+
+driver_revenue as (
+        
+    select
+        driver_id,
+        30.437*(driver_rides.revenue/TIMESTAMP_DIFF(driver_rides.most_recent_ride_date, drivers.driver_onboard_date, day)) as avg_revenue_month,
+        5.115151515151515 as lifetime_churn
+            --avg lifetime of driver based on churn (calculated based on # of drivers active in March and # of drivers active in June)
+
+    from driver_rides
+    join drivers using (driver_id)
 
 ),
 
@@ -55,12 +66,18 @@ final as (
         driver_rides.first_ride_date,
         driver_rides.most_recent_ride_date,
         coalesce(driver_rides.number_of_rides, 0) as number_of_rides,
-        coalesce(driver_rides.lifetime_value, 0) as lifetime_value
-
+        coalesce(driver_rides.revenue, 0) as revenue,
+        TIMESTAMP_DIFF(driver_rides.most_recent_ride_date, drivers.driver_onboard_date, day) as tenure,
+        driver_revenue.lifetime_churn,
+        driver_revenue.avg_revenue_month,
+        ROUND(driver_revenue.lifetime_churn * driver_revenue.avg_revenue_month, 2) as lifetime_value
+        
     from drivers
     
     left join driver_rides using (driver_id)
+    left join driver_revenue using (driver_id)
 
 )
 
 select * from final
+ORDER BY avg_revenue_month
--- models/marts/core/fct_events.sql
+++ models/marts/core/fct_events.sql
@@ -29,6 +29,7 @@ ride_events as (
         events.ride_id,
         events.event,
         events.timestamp,
+        CASE WHEN event = 'accepted_at' THEN DATE_TRUNC(timestamp, day) END AS ride_date,
         DATETIME(events.timestamp, '-8:00') as timestamp_pst,
         (-1 * (events.seconds - LEAD(events.seconds, 1) OVER
             (PARTITION BY events.ride_id ORDER BY events.timestamp)))
